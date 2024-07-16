0ee7c768cd6067fa0ee0930ee24ded4051d8b521
Sanowar72 <atifreza72ar@gmail.com>
2024-07-16 23:30:18 +0530

    calling component dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import Hello from "./component/Hello";
 import Name from "./component/Name";
 
 function App() {
- 
+  const myarr = [
+    { name: "sai", age: 20 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+    { name: "thor", age: 40 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+    { name: "tony", age: 28 },
+    { name: "steve", age: 30 },
+    { name: "bruce", age: 35 },
+  ];
   return (
     <>
       <h1>this is app jsx </h1>
-      <Name name="sonu" rollNo="17" />
-      <Name name="tony" rollNo="19" />
-      <Name name="sony" rollNo="35" />
-      <Name name="shibonu" rollNo="45" />
+      {myarr.map((item) => (
+        <Name name={item.name} age={item.age} />
+      ))}
     </>
   );
 }
--- src/component/Name.jsx
+++ src/component/Name.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Name = ({ name, rollNo }) => {
+const Name = ({ name, age }) => {
   return (
     <div>
       <h1>your name is : {name}</h1>
-      <h3>your roll no : {rollNo}</h3>
+      <h3>your age is : {age}</h3>
     </div>
   );
 };
