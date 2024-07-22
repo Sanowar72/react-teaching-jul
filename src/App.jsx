91762335e389748fb2dd57d315363b8991195612
Sanowar72 <atifreza72ar@gmail.com>
2024-07-22 21:25:30 +0530

    optional chaining added

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,22 @@ import Name from "./component/Name";
 
 function App() {
   const myarr = [
-    { name: "sai", age: 20 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
-    { name: "thor", age: 40 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
-    { name: "tony", age: 28 },
-    { name: "steve", age: 30 },
-    { name: "bruce", age: 35 },
+    { name: "sai", age: 20, fatherName: "bruce", email: "hsbb@gamil.com" },
+    { name: "prasad", age: 20, email: "hsbb@gamil.com" },
+    { name: "priya", age: 20, fatherName: "bruce" },
+    { name: "wayne", age: 20, fatherName: "bruce", email: "hsbb@gamil.com" },
+    { name: "random", age: 20, email: "hsbb@gamil.com" },
   ];
   return (
     <>
       <h1>this is app jsx </h1>
       {myarr.map((item) => (
-        <Name name={item.name} age={item.age} />
+        <Name
+          name={item.name}
+          age={item.age}
+          fatherName={item.fatherName}
+          email={item.email}
+        />
       ))}
     </>
   );
--- src/component/Name.jsx
+++ src/component/Name.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Name = ({ name, age }) => {
+const Name = ({ name, age, fatherName, email }) => {
   return (
     <div>
       <h1>your name is : {name}</h1>
       <h3>your age is : {age}</h3>
+      <h1>father name is :{fatherName?.toUpperCase() || "no data provided"}</h1>
+      <h2>ypur email is : {email}</h2>
     </div>
   );
 };
