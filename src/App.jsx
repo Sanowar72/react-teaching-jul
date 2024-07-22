91142b6b119ef0d1a611cdcb6c01b6ebc5d76570
Sanowar72 <atifreza72ar@gmail.com>
2024-07-22 21:30:20 +0530

    conditional rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import React, { useState } from "react";
-import Hello from "./component/Hello";
-import Name from "./component/Name";
+
 
 function App() {
-  const myarr = [
-    { name: "sai", age: 20, fatherName: "bruce", email: "hsbb@gamil.com" },
-    { name: "prasad", age: 20, email: "hsbb@gamil.com" },
-    { name: "priya", age: 20, fatherName: "bruce" },
-    { name: "wayne", age: 20, fatherName: "bruce", email: "hsbb@gamil.com" },
-    { name: "random", age: 20, email: "hsbb@gamil.com" },
-  ];
+  const age = 20;
   return (
     <>
       <h1>this is app jsx </h1>
-      {myarr.map((item) => (
-        <Name
-          name={item.name}
-          age={item.age}
-          fatherName={item.fatherName}
-          email={item.email}
-        />
-      ))}
+      {age > 18 ? "you are eligible to vote" : "you r not eligible to vote"}
     </>
   );
 }
